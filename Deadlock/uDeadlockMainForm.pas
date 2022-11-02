adbc590f8f980569823349709e7f4976f1b13f90
isuru_s <rmx.sas@gmail.com>
2022-11-02 14:28:28 +0530

    deadlock; bug in range check

--- Deadlock/uDeadlockMainForm.pas
+++ Deadlock/uDeadlockMainForm.pas
@@ -62,7 +62,6 @@ begin
   inherited Create(True);
   FOwner := AOwner;
   FThreadNo := AThreadNo;
-  // create critical section
   FMessageQueue.FCriticalSec := TCriticalSection.Create;
 end;
 
@@ -74,35 +73,96 @@ begin
 end;
 
 procedure TSeperateThread.Execute;
+Var
+  I : Integer;
+  ACounter : Integer;
+  NextReceiver : Integer;
+  Receiver : TSeperateThread;
 begin
+
+  while (not Terminated) do Begin
+
+    FMessageQueue.FCriticalSec.Enter();
+
+    if (Random(10) = 1) then Begin
+      SetLength(FMessageQueue.FMessages, Length(FMessageQueue.FMessages) + 1);
+      while True do begin
+        if NextReceiver <> FThreadNo then begin
+          NextReceiver := Random(THREAD_COUNT);
+          FMessageQueue.FMessages[Length( FMessageQueue.FMessages ) - 1].FChannelNo := NextReceiver;
+          break;
+        end; end;
+    End;
+
+    Sleep(2);
+
+    for I := 0 to (THREAD_COUNT - 1) do Begin
+      // Get the current thread as receiver
+      Receiver := FOwner.TMyThread[FMessageQueue.FMessages[I].FChannelNo];
+      // put receiver thread into critical section
+      Receiver.FMessageQueue.FCriticalSec.Enter();
+      // Set message Queue length
+      SetLength(Receiver.FMessageQueue.FMessages, Length(Receiver.FMessageQueue.FMessages) + 1 );
+      // set Message buffer length
+      SetLength(Receiver.FMessageQueue.FMessages[Length(Receiver.FMessageQueue.FMessages) - 1].FMessageBuffer, Length(FMessageQueue.FMessages[I].FMessageBuffer));
+      // copy one memory portion to another
+           // source                                     // destination                                                                  // size
+      Move(FMessageQueue.FMessages[I].FMessageBuffer[0], Receiver.FMessageQueue.FMessages[Length(Receiver.FMessageQueue.FMessages) - 1], Length(FMessageQueue.FMessages[I].FMessageBuffer));
+
+
+      while True do begin
+      if (NextReceiver <> FThreadNo) then begin
+        NextReceiver := Random(THREAD_COUNT);
+        Receiver.FMessageQueue.FMessages[Length( Receiver.FMessageQueue.FMessages ) - 1].FChannelNo := NextReceiver;
+        break;
+        end;
+      end;
+
+      Receiver.FMessageQueue.FCriticalSec.Leave();
+    End;
+
+    SetLength(FMessageQueue.FMessages, 0);
+    FMessageQueue.FCriticalSec.Leave();
+
+    // sync with main thread
+    Synchronize(
+       procedure() begin
+         FOwner.ListBx.Items[FThreadNo] := IntToStr(ACounter);
+       end
+    );
+
+    Inc( ACounter );
+    Sleep( Random( 30 ) );
+
+  End;
+
 
 end;
 
-
-
 procedure TForm6.FormCreate(Sender: TObject);
-var I : Integer;
+var I: Integer;
 begin
 
-  for I := 0 to THREAD_COUNT - 1 do Begin
+  // populate listbox & creates multiple threads
+  for I := 0 to THREAD_COUNT- 1 do Begin
     ListBx.Items.Add('0');
-    // populate listbox with thread
     TMyThread[I] := TSeperateThread.Create(Self, I);
   End;
 
   // execute each thread
   for I := 0 to THREAD_COUNT - 1 do
-    TMyThread[I].Start;
+    TMyThread[I].Start();
 
 end;
 
 procedure TForm6.FormDestroy(Sender: TObject);
-var I : Integer;
+var I,J : Integer;
 begin
   // terminate all threads
   for I := 0 to THREAD_COUNT -1  do Begin
-    TMyThread[I].Terminate;
-    TMyThread[I].WaitFor;
+    TMyThread[I].Terminate();
+                 // wait for the thread to terminate
+    TMyThread[I].WaitFor();
   End;
 
   // release threads
