8d0f4ceef2439016c836cd635340a7a4650d2f86
isuru_s <rmx.sas@gmail.com>
2022-11-03 14:33:13 +0530

    Deadlock application; bug fixed

--- Deadlock/uDeadlockMainForm.dfm
+++ Deadlock/uDeadlockMainForm.dfm
@@ -3,15 +3,13 @@ object Form6: TForm6
   Top = 0
   Caption = 'DeadLock '
   ClientHeight = 321
-  ClientWidth = 367
+  ClientWidth = 377
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
   Font.Height = -12
   Font.Name = 'Segoe UI'
   Font.Style = []
-  OnCreate = FormCreate
-  OnDestroy = FormDestroy
   PixelsPerInch = 96
   TextHeight = 15
   object lblDescription: TLabel
@@ -22,12 +20,42 @@ object Form6: TForm6
     Caption = 'Seperate Thread  / Deadlock situation'
   end
   object ShpStatus: TShape
-    Left = 256
-    Top = 104
-    Width = 73
-    Height = 81
+    Left = 280
+    Top = 8
+    Width = 33
+    Height = 49
     Shape = stCircle
     Visible = False
+  end
+  object ShpAllThreadsStarted: TShape
+    Left = 240
+    Top = 82
+    Width = 33
+    Height = 14
+    Brush.Color = clSilver
+    Shape = stRoundRect
+  end
+  object lblThreadRunning: TLabel
+    Left = 240
+    Top = 102
+    Width = 41
+    Height = 14
+    Caption = 'Threads'
+  end
+  object lblCriticalSec: TLabel
+    Left = 240
+    Top = 178
+    Width = 30
+    Height = 15
+    Caption = 'Locks'
+  end
+  object ShpCriticalSec: TShape
+    Left = 240
+    Top = 162
+    Width = 33
+    Height = 15
+    Brush.Color = clSilver
+    Shape = stRoundRect
   end
   object ListBx: TListBox
     Left = 24
@@ -36,5 +64,24 @@ object Form6: TForm6
     Height = 201
     ItemHeight = 15
     TabOrder = 0
+  end
+  object btnExecute: TButton
+    Left = 24
+    Top = 280
+    Width = 97
+    Height = 25
+    Caption = 'Run'
+    TabOrder = 1
+    OnClick = btnExecuteClick
+  end
+  object btnStop: TButton
+    Left = 136
+    Top = 280
+    Width = 84
+    Height = 25
+    Caption = 'Stop'
+    Enabled = False
+    TabOrder = 2
+    OnClick = btnStopClick
   end
 end
--- Deadlock/uDeadlockMainForm.pas
+++ Deadlock/uDeadlockMainForm.pas
@@ -4,10 +4,10 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, SyncObjs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, SyncObjs, System.Math;
 
 const
-  THREAD_COUNT = 10;
+  THREAD_COUNT = 13;
 
 type
   TForm6 = class;
@@ -24,25 +24,35 @@ type
 
   TSeperateThread = class(TThread)
   private
-    FOwner    : TForm6;
-    FThreadNo : Integer;
+    FOwner      : TForm6;
+    FThreadNo   : Integer;
     FMessageQueue : TMessageQueue;
   protected
     procedure Execute(); override;
   public
     constructor Create(AOwner: TForm6; AThreadNo : Integer);
-    destructor Destroy(); override;
+    destructor  Destroy(); override;
   end;
 
   TForm6 = class(TForm)
     lblDescription: TLabel;
     ShpStatus: TShape;
     ListBx: TListBox;
-    procedure FormCreate(Sender: TObject);
-    procedure FormDestroy(Sender: TObject);
+    ShpAllThreadsStarted: TShape;
+    lblThreadRunning: TLabel;
+    btnExecute: TButton;
+    btnStop: TButton;
+    lblCriticalSec: TLabel;
+    ShpCriticalSec: TShape;
+    procedure btnExecuteClick(Sender: TObject);
+    procedure btnStopClick(Sender: TObject);
   private
     { Private declarations }
-    TMyThread : array [0..THREAD_COUNT-1] of TSeperateThread;
+    FLocksCount : Integer;
+    FThreadCount : Integer;
+    FMyThread : array [0..THREAD_COUNT-1] of TSeperateThread;
+
+    procedure StatusUpdate;
   public
     { Public declarations }
   end;
@@ -59,116 +69,171 @@ implementation
 
 constructor TSeperateThread.Create(AOwner: TForm6; AThreadNo: Integer);
 begin
-  inherited Create(True);
+  inherited Create(true);
   FOwner := AOwner;
   FThreadNo := AThreadNo;
+  // one critical section for one thread
   FMessageQueue.FCriticalSec := TCriticalSection.Create;
 end;
 
 destructor TSeperateThread.Destroy;
 begin
-  // release critical section
   FMessageQueue.FCriticalSec.Free;
   inherited;
 end;
 
 procedure TSeperateThread.Execute;
-Var
+var
   I : Integer;
   ACounter : Integer;
-  NextReceiver : Integer;
-  Receiver : TSeperateThread;
+  NextReceiverNo : Integer;
+  ReceiverThread : TSeperateThread;
 begin
-
-  while (not Terminated) do Begin
-
+  while not Terminated do Begin
+    // put message queue in critical section
     FMessageQueue.FCriticalSec.Enter();
 
-    if (Random(10) = 1) then Begin
+    {__________________________________________________________________________}
+    if Random(10) = 1 then begin
       SetLength(FMessageQueue.FMessages, Length(FMessageQueue.FMessages) + 1);
       while True do begin
-        if NextReceiver <> FThreadNo then begin
-          NextReceiver := Random(THREAD_COUNT);
-          FMessageQueue.FMessages[Length( FMessageQueue.FMessages ) - 1].FChannelNo := NextReceiver;
+        // randomly assing no among thread no
+        NextReceiverNo := Random(THREAD_COUNT);
+        if NextReceiverNo <> FThreadNo then begin
+          // set queue's -> message list item's -> message nnumber
+          FMessageQueue.FMessages[Length(FMessageQueue.FMessages)-1].FChannelNo := NextReceiverNo;
           break;
-        end; end;
-    End;
+        end;
+      end;
+    end;
 
-    Sleep(2);
+    Sleep(1);
+    
+    {__________________________________________________________________________}
+    for I := 0 to Length(FMessageQueue.FMessages) - 1 do Begin
+      // get one by one thread
+      ReceiverThread := Form6.FMyThread[FMessageQueue.FMessages[I].FChannelNo]; 
 
-    for I := 0 to (THREAD_COUNT - 1) do Begin
-      // Get the current thread as receiver
-      Receiver := FOwner.TMyThread[FMessageQueue.FMessages[I].FChannelNo];
-      // put receiver thread into critical section
-      Receiver.FMessageQueue.FCriticalSec.Enter();
-      // Set message Queue length
-      SetLength(Receiver.FMessageQueue.FMessages, Length(Receiver.FMessageQueue.FMessages) + 1 );
-      // set Message buffer length
-      SetLength(Receiver.FMessageQueue.FMessages[Length(Receiver.FMessageQueue.FMessages) - 1].FMessageBuffer, Length(FMessageQueue.FMessages[I].FMessageBuffer));
-      // copy one memory portion to another
-           // source                                     // destination                                                                  // size
-      Move(FMessageQueue.FMessages[I].FMessageBuffer[0], Receiver.FMessageQueue.FMessages[Length(Receiver.FMessageQueue.FMessages) - 1], Length(FMessageQueue.FMessages[I].FMessageBuffer));
+      // put receiver's message queue into critical section
+      ReceiverThread.FMessageQueue.FCriticalSec.Enter(); 
+      // receiver thread's message queue's length
+      SetLength(ReceiverThread.FMessageQueue.FMessages, Length(ReceiverThread.FMessageQueue.FMessages) + 1);
+      // set receiver's message beffer's length
+      SetLength(ReceiverThread.FMessageQueue.FMessages[Length(ReceiverThread.FMessageQueue.FMessages) - 1].FMessageBuffer, Length( FMessageQueue.FMessages[I].FMessageBuffer ));
+      // Copy a section of memory from one place to another
+      Move( FMessageQueue.FMessages[ I ].FMessageBuffer[ 0 ], ReceiverThread.FMessageQueue.FMessages[ Length( ReceiverThread.FMessageQueue.FMessages ) - 1 ].FMessageBuffer[ 0 ], Length( FMessageQueue.FMessages[ I ].FMessageBuffer ));  
 
-
-      while True do begin
-      if (NextReceiver <> FThreadNo) then begin
-        NextReceiver := Random(THREAD_COUNT);
-        Receiver.FMessageQueue.FMessages[Length( Receiver.FMessageQueue.FMessages ) - 1].FChannelNo := NextReceiver;
-        break;
+      While True do begin
+        NextReceiverNo := Random(THREAD_COUNT);
+        if NextReceiverNo <> FThreadNo then begin
+          // set receiver's -> queue's -> message list item's -> message nnumber
+          ReceiverThread.FMessageQueue.FMessages[Length(ReceiverThread.FMessageQueue.FMessages)-1].FChannelNo := NextReceiverNo;
+          break;
         end;
       end;
 
-      Receiver.FMessageQueue.FCriticalSec.Leave();
+      // release receiver's message thread's lock
+      ReceiverThread.FMessageQueue.FCriticalSec.Leave();
     End;
+    
+   {___________________________________________________________________________}
+   SetLength(FMessageQueue.FMessages, 0);
 
-    SetLength(FMessageQueue.FMessages, 0);
-    FMessageQueue.FCriticalSec.Leave();
+   // update form's listbox via sync method
+   Synchronize(
+     procedure() Begin
+       Form6.ListBx.Items[ FThreadNo ] := IntToStr(ACounter);
+     End
+   ); 
+    
+   Inc(ACounter);
+   Sleep(Random(30));
+  End;
+end;
 
-    // sync with main thread
-    Synchronize(
-       procedure() begin
-         FOwner.ListBx.Items[FThreadNo] := IntToStr(ACounter);
-       end
-    );
+procedure TForm6.btnExecuteClick(Sender: TObject);
+var I : Integer;
+begin
+  btnExecute.Enabled := False;
+  btnStop.Enabled := True;
 
-    Inc( ACounter );
-    Sleep( Random( 30 ) );
-
+  // populate list box & create multiple threads for the form
+  for I := 0 to THREAD_COUNT - 1 do Begin
+    ListBx.Items.Add( '0' );
+    FMyThread[I] := TSeperateThread.Create( Self,I );
   End;
 
+  for I := 0 to THREAD_COUNT-1 do Begin
+    FMyThread[I].Start;
+  End;
+
+  Self.StatusUpdate;
 
 end;
 
-procedure TForm6.FormCreate(Sender: TObject);
-var I: Integer;
+procedure TForm6.btnStopClick(Sender: TObject);
+var I : Integer;
 begin
+  btnStop.Enabled := False;
+  btnExecute.Enabled := True;
 
-  // populate listbox & creates multiple threads
-  for I := 0 to THREAD_COUNT- 1 do Begin
-    ListBx.Items.Add('0');
-    TMyThread[I] := TSeperateThread.Create(Self, I);
-  End;
+  // terminate all the threads in the form
+  for I := 0 to THREAD_COUNT - 1 do begin
+    FMyThread[I].Terminate;
+    FMyThread[I].WaitFor;
+  end;
 
-  // execute each thread
-  for I := 0 to THREAD_COUNT - 1 do
-    TMyThread[I].Start();
+  for I := 0 to THREAD_COUNT - 1 do begin
+    FMyThread[I].Free;
+  end;
 
+  ListBx.Items.Clear;
+
+  // bugs in locks!
+  Self.StatusUpdate;
 end;
 
-procedure TForm6.FormDestroy(Sender: TObject);
-var I,J : Integer;
+procedure TForm6.StatusUpdate;
+Var I : Integer;
+   Msg: String;
 begin
-  // terminate all threads
-  for I := 0 to THREAD_COUNT -1  do Begin
-    TMyThread[I].Terminate();
-                 // wait for the thread to terminate
-    TMyThread[I].WaitFor();
+
+  // thread count & lock count in the form
+  for I := 0 to THREAD_COUNT - 1 do Begin
+    
+    if FMyThread[I] <> NIL then Begin
+      if not FMyThread[I].Terminated then
+        Inc(FThreadCount)
+      else
+        Dec(FThreadCount);
+
+      if (FMyThread[I].FMessageQueue.FCriticalSec) <> NIL then
+        Inc(FLocksCount)
+      else
+        Dec(FLocksCount);    
+    End;
+      
   End;
 
-  // release threads
-  for I := 0 to THREAD_COUNT - 1 do
-    TMyThread[I].Free;
-
+  // thread status
+  if FThreadCount = THREAD_COUNT then Begin
+    lblThreadRunning.Caption := 'Threads: All Running';
+    ShpAllThreadsStarted.Brush.Color := clLime;
+  End else Begin
+    lblThreadRunning.Caption := 'Threads: Running error!';
+    ShpAllThreadsStarted.Brush.Color := clRed;   
+  End;
+  
+  // locks status
+  if FLocksCount = THREAD_COUNT then Begin
+//    lblCriticalSec.Caption := 'Locks: All Applied';
+//    ShpCriticalSec.Brush.Color := clLime;
+  End else Begin
+//    lblCriticalSec.Caption := 'Locks: Locks Error!';
+//    ShpCriticalSec.Brush.Color := clRed;
+  End;
+  
+  
 end;
 
 end.
